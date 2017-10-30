bb3b87a71f0a8ff93b0c4223edb08365f9497bdd
Marcos A. Avila <marcosa@uci.edu>
2017-10-29 21:48:49 -0700

    Pit detection now works. Will calculate probability of pit at any given cell c:

--- src/MyAI.java
+++ src/MyAI.java
@@ -19,20 +19,29 @@
 //                be lost when the tournament runs your code.
 // ======================================================================
 
+import java.lang.Math;
+import java.util.Scanner;
+
 public class MyAI extends Agent
 {
 	private class Cell
 	{
-		private double pitProb;
-		private double wumpProb;
+		public double pitProb;
+		public double wumpProb;
+		public boolean stench;
+		public boolean breeze;
+		public boolean explored;
 
 		public Cell()
 		{
 			pitProb = -1;
 			wumpProb = -1;
+			stench = false;
+			breeze = false;
+			explored = false;
 		}
 
-		public void setPitProb(double pProb)
+/*		public void setPitProb(double pProb)
 		{
 			pitProb = pProb;
 		}
@@ -51,12 +60,7 @@ public class MyAI extends Agent
 		{
 			return wumpProb;
 		}
-
-	}
-
-	private enum Direction
-	{
-		UP, RIGHT, DOWN, LEFT
+*/
 	}
 
 	private Cell[][] agentMap;
@@ -65,7 +69,7 @@ public class MyAI extends Agent
 
 	private int curRow;
 	private int curCol;
-	private Direction curDir;
+	private int curDir;
 
 	public MyAI ( )
 	{
@@ -83,22 +87,97 @@ public class MyAI extends Agent
 			for(int col = 0; col < mapCols; ++col)
 				agentMap[row][col] = new Cell();
 
-		agentMap[0][0].setPitProb(0);	
-		agentMap[0][0].setWumpProb(0);	
-
 		curRow = 0;
 		curCol = 0;
-		curDir = Direction.RIGHT; 
+		curDir = 0; 
 		// ======================================================================
 		// YOUR CODE ENDS
 		// ======================================================================
 	}
 
-/*	public Cell[] getAdjacentCells(int r, int c)
+	private void checkPitLocalConsistency(int row, int col)
 	{
-		new
+		// I know... really gross, but it's helpful.
+		int diagCellRow;
+		int diagCellCol;
+		int adjCellRow1;
+		int adjCellCol1;
+		int possiblePitCount;
+		int adjCellRow2;
+		int adjCellCol2;
+
+		// First loop iterates through diagonal cells
+		for(int n = 0; n < 4; ++n)
+		{
+			diagCellRow = row + (int) (Math.cos(Math.PI / 2 * n)) + (int) (Math.sin(Math.PI / 2 * n));
+			diagCellCol = col + (int) (Math.cos(Math.PI / 2 * (n + 1))) + (int) (Math.sin(Math.PI / 2 * (n + 1)));
+			System.out.printf("%d, %d\n", diagCellRow, diagCellCol);
+
+			if(diagCellRow < 0 || diagCellCol < 0 || diagCellRow > 3 || diagCellCol > 9 || agentMap[diagCellRow][diagCellCol].pitProb <= 0)
+				continue;
+
+			// Second loop iterates through cells adjacent to the diagonals and checks to see if they have a breeze
+			for(int m = 0; m < 4; ++m)
+			{
+				adjCellRow1 = diagCellRow + (int) Math.sin(Math.PI / 2 * m);
+				adjCellCol1 = diagCellCol + (int) Math.cos(Math.PI / 2 * m);
+
+				if(adjCellRow1 < 0 || adjCellCol1 < 0 || adjCellRow1 > 3 || adjCellCol1 > 9 || !agentMap[adjCellRow1][adjCellCol1].breeze)
+					continue;
+
+				possiblePitCount = 0;
+				// Last loop iterates around the cells with breezes and counts the number of possible pits around it
+				for(int l = 0; l < 4; ++l)
+				{
+					adjCellRow2 = adjCellRow1 + (int) Math.sin(Math.PI / 2 * l);
+					adjCellCol2 = adjCellCol1 + (int) Math.cos(Math.PI / 2 * l);
+					if(adjCellRow2 < 0 || adjCellCol2 < 0 || adjCellRow2 > 3 || adjCellCol2 > 9)
+						continue;
+
+					possiblePitCount += (agentMap[adjCellRow2][adjCellCol2].pitProb != 0) ? 1 : 0;
+					// If only 1 possible pit, then update that pit probability to 1
+				}
+				
+				if(possiblePitCount == 1)
+				{
+					agentMap[diagCellRow][diagCellCol].pitProb = 1;
+					System.out.printf("row: %d, col: %d\n", adjCellRow1, adjCellCol1);
+				}
+
+			}
+		}
 	}
-*/
+	
+	private void updatePitProb(int row, int col)
+	{
+		int adjCellRow;
+		int adjCellCol;
+
+		int adjCellCount = 0;
+		int breezeCount = 0;
+		
+		if(agentMap[row][col].pitProb == 0 || agentMap[row][col].pitProb == 1)
+			return;	
+
+		for(int n = 0; n < 4; ++n)
+		{
+			adjCellRow = row + (int) Math.sin(Math.PI / 2 * n);
+			adjCellCol = col + (int) Math.cos(Math.PI / 2 * n);
+
+			if(adjCellRow < 0 || adjCellCol < 0 || adjCellRow > 3 || adjCellCol > 9)
+				continue;
+
+			++adjCellCount;
+			breezeCount += (agentMap[adjCellRow][adjCellCol].breeze) ? 1: 0;
+		}
+
+		agentMap[row][col].pitProb = ((double)breezeCount) / adjCellCount;
+		
+	}
+	public void updateWumpProb(){
+	}
+
+
 	public void updateMap
 	(
 		boolean stench,
@@ -106,21 +185,74 @@ public class MyAI extends Agent
 		boolean bump
 	)
 	{
+		int adjCellRow;
+		int adjCellCol;		
+		
+		agentMap[curRow][curCol].pitProb = 0;
+		agentMap[curRow][curCol].wumpProb = 0;
+		agentMap[curRow][curCol].stench = stench;
+		agentMap[curRow][curCol].breeze = breeze;
+
+		// bump tells us that we hit a wall, record this number
 		if(bump)
-			if(curDir == Direction.RIGHT)
+		{
+			if(curDir == 0)
 				mapCols = curCol + 1;
-			else if(curDir == Direction.UP)
+			else if(curDir == 3)
 				mapRows = curRow + 1;
-		//for()
+			return;
+		}
+
+		// If previously explored
+		if(agentMap[curRow][curCol].explored)
+			return;
+
+		System.out.println("Oh snaapp it's gonna do the thing...");
+		checkPitLocalConsistency(curRow, curCol);
+		System.out.println("Oh snaapp it did the thing...");
+		
+		// If stench or breeze, try and figure out where the wumpus or pit is
+		for(int n = 0; n < 4; ++n)
+		{
+			adjCellRow = curRow + (int) Math.sin(Math.PI / 2 * n);
+			adjCellCol = curCol + (int) Math.cos(Math.PI / 2 * n);
+
+			if (adjCellRow < 0 || adjCellCol < 0 || adjCellRow > 3 || adjCellCol > 9)
+				continue;
+
+//			agentMap[adjCellRow][adjCellCol].pitProb = 0;
+//			agentMap[adjCellRow][adjCellCol].wumpProb = 0;
+			
+
+			if(breeze)
+				updatePitProb(adjCellRow, adjCellCol);
+			else
+			{
+				if(agentMap[adjCellRow][adjCellCol].pitProb > 0)
+				{
+					System.out.println("Oh snaapp it's gonna do the thing...");
+					checkPitLocalConsistency(adjCellRow, adjCellCol);
+					System.out.println("Oh snaapp it did the thing...");
+				}
+				agentMap[adjCellRow][adjCellCol].pitProb = 0;
+			}
+/*			if(stench)
+			{
+//				agentMap[adjCellRow][adjCellCol].wumpProb += 0.25;
+			}
+*/
+		}
+
+		agentMap[curRow][curCol].explored = true;
 	}
-	
+
 	public void printAgentMap()
 	{
 		for(int row = 3; row >=0; --row)
 		{
 			for(int col = 0; col < 10; ++col)
 			{
-				System.out.printf("%6.1f,%-4.1f", agentMap[row][col].getPitProb(),agentMap[row][col].getWumpProb());
+				System.out.printf("%7.2f,%-5.2f", agentMap[row][col].pitProb, agentMap[row][col].wumpProb);
 			}
 			System.out.println();
 		}
@@ -138,8 +270,50 @@ public class MyAI extends Agent
 		// ======================================================================
 		// YOUR CODE BEGINS
 		// ======================================================================
-
+		updateMap(stench, breeze, bump);
 		printAgentMap();
+
+		
+		System.out.println ( "Press 'w' to Move Forward  'a' to 'Turn Left' 'd' to 'Turn Right'" );
+		System.out.println ( "Press 's' to Shoot         'g' to 'Grab'      'c' to 'Climb'" );
+		
+		Scanner in = new Scanner(System.in);
+		// Get Input
+		System.out.print ( "Please input: " );
+		String userInput = in.next();
+
+		// Return Action Associated with Input
+		if ( userInput.charAt(0) == 'w' )
+		{
+			if ( curDir == 0 )
+				++curCol;
+			else if ( curDir == 1 )
+				--curRow;
+			else if ( curDir == 2 )
+				--curCol;
+			else
+				++curRow;
+			return Action.FORWARD;
+		}
+
+		if ( userInput.charAt(0) == 'a' )
+		{
+			if (--curDir < 0) curDir = 3;	
+			return Action.TURN_LEFT;
+		}
+
+		if ( userInput.charAt(0) == 'd' )
+		{
+			if (++curDir > 3) curDir = 0;
+			return Action.TURN_RIGHT;
+		}
+
+		if ( userInput.charAt(0) == 's' )
+			return Action.SHOOT;
+
+		if ( userInput.charAt(0) == 'g' )
+			return Action.GRAB;
+		
 		return Action.CLIMB;
 		// ======================================================================
 		// YOUR CODE ENDS
