0d57aab0875c16f318487f7bd9596029c89fdaa2
DaiconV <marcosa@uci.edu>
2017-11-22 21:32:26 -0800

    Update MyAI.java

--- src/MyAI.java
+++ src/MyAI.java
@@ -22,7 +22,10 @@
 
 import java.util.ArrayList;
 import java.util.Stack;
-
+import java.lang.Math;
+import java.util.Scanner;
+import java.util.ArrayDeque;
+import java.util.HashSet;
 
 
 
@@ -57,15 +60,105 @@ public class MyAI extends Agent
 			//End MA 10/30
 		}
 	}
+public class SimpleCell
+	{
+		public int row;
+		public int col;
+
+		public SimpleCell()
+		{
+			row = -1;
+			col = -1;
+		}
+
+		public SimpleCell(int r, int c)
+		{
+			row = r;
+			col = c;
+		}
+
+		@Override
+		public int hashCode()
+		{
+			return row*100 + col;
+		}
+
+		@Override
+		public boolean equals(Object obj)
+		{
+			if(this == obj)
+				return true;
+			if(obj == null || obj.getClass() != this.getClass())
+				return false;
+			
+			SimpleCell cell = (SimpleCell) obj;
+			
+			return	(row == cell.row) && 
+					(col == cell.col);
+		}
+
+		public String toString()
+		{
+			return String.format("Cell at: %d, %d", row, col);
+		}
+	}
+
+	public class State extends SimpleCell
+	{
+		public int dir;
+		
+		public State()
+		{
+			super();
+			dir = -1;
+		}
+
+		public State(int r, int c, int d)
+		{
+			super(r, c);
+			dir = d;
+		}
+		@Override
+		public int hashCode()
+		{
+			return dir*10000 + row*100 + col;
+		}
+
+		@Override
+		public boolean equals(Object obj)
+		{
+			if(this == obj)
+				return true;
+			if(obj == null)
+				return false;
+			
+			State state = (State) obj;
+
+			if(obj.getClass() == this.getClass())
+				return super.equals(state) && (dir == state.dir);
+			else if(obj.getClass() == super.getClass())
+				return super.equals(state);
+
+			return false;
+		}
+		@Override
+		public String toString()
+		{
+			return String.format("State: %d, %d, %d", row, col, dir);
+		}
+	}
 
 	private enum Direction
 	{
 		UP, RIGHT, DOWN, LEFT
 	}
-
-	private Cell[][] agentMap;
+	
 	private int mapRows;
 	private int mapCols;
+	
+	private Cell[][] agentMap;
+	private ArrayDeque<Integer> commandQueue;
+	private HashSet<SimpleCell> goalCells;	
 
 	private int curRow;
 	private int curCol;
@@ -565,449 +658,129 @@ public class MyAI extends Agent
 		noWumpusConfirmed(curRow, curCol);
 	}
 
-	private int oscillationFunction(int n)
-	// This function oscillates in the form of 1, 0, -1, 0, 1, ....
-	{
-		return (int) (Math.cos(Math.PI / 2 * n));
-	}
-
-/*
-*******************Wumpus Detection Logic*******************
-*/
-
-	private void wumpusConfirmed(int row, int col)
-	{
-		if(agentMap[row][col].wumpProb != 1)
-			return;
-
-		wumpusFound = true;
-		for(int i = 0; i < mapRows; ++i)
-			for(int j = 0; j < mapCols; ++j)
-			{
-				if(i == row && j == col)
-					continue;
-				agentMap[i][j].wumpProb = 0;
-			}
-	}
-	
-	private void noWumpusConfirmed(int row, int col)
-	//Should only call on cells that have been confirmed as non-pits.
-	//This function performs some logical consistency checks on this world.
-	{
-//		System.out.printf("Cell: row %d; col %d\n", row, col);		
-		if(agentMap[row][col].wumpProb != 0)
-			return;
-
-		int adjRow;
-		int adjCol;
-		for (int n = 0; n < 4; ++n)
-		{
-			if(wumpusFound)
-				return;
-
-			adjRow = row + oscillationFunction(n);
-			adjCol = col + oscillationFunction(n + 1);
-			if(adjRow < 0 || adjRow >= mapRows || adjCol < 0 || adjCol >= mapCols)
-				continue;
-			
-//			System.out.printf("First Cells: row %d; col %d\n", adjRow1, adjCol1);
-
-			checkStenchConsistency(adjRow, adjCol);
-		}
-	}
-
-	private void checkStenchConsistency(int row, int col)
-	{
-		int potentialWumpusRow = -1;
-		int potentialWumpusCol = -1;
-		int potentialWumpusCount = 0;
-		
-		if(!agentMap[row][col].stench)
-			return;
-				
-		int adjRow;
-		int adjCol;
-		for (int n = 0; n < 4; ++n)
-		{
-			adjRow = row + oscillationFunction(n);
-			adjCol = col + oscillationFunction(n + 1);
-
-			if(adjRow < 0 || adjRow >= mapRows || adjCol < 0 || adjCol >= mapCols)
-				continue;
-
-			if(agentMap[adjRow][adjCol].wumpProb != 0)
-			{
-				potentialWumpusRow = adjRow;
-				potentialWumpusCol = adjCol;
-				++potentialWumpusCount;
-			}
-
-		}
-		if(potentialWumpusCount == 1)
-		{
-			agentMap[potentialWumpusRow][potentialWumpusCol].wumpProb = 1;
-			wumpusConfirmed(potentialWumpusRow, potentialWumpusCol);
-		}
-	}
-
-	private void updateWumpusProbability(int row, int col)
-	{
-		int adjCellCount = 0;
-		int stenchCount = 0;
-
-		if(agentMap[row][col].wumpProb == 0)
-			return;
-
-		int adjRow;
-		int adjCol;
-		for (int n = 0; n < 4; ++n)
-		{
-			adjRow = row + oscillationFunction(n);
-			adjCol = col + oscillationFunction(n + 1);
-
-			if(adjRow < 0 || adjRow >= mapRows || adjCol < 0 || adjCol >= mapCols)
-				continue;
-
-			++adjCellCount;			
-			if(agentMap[adjRow][adjCol].stench)
-				++stenchCount;
-
-			if(agentMap[adjRow][adjCol].explored && !agentMap[adjRow][adjCol].stench)
-			{
-				agentMap[row][col].wumpProb = 0;
-				noWumpusConfirmed(row, col);
-				return;
-			}
-//			System.out.printf("Cell: %d, %d\n", adjRow, adjCol);
-		}
-
-		if(stenchCount == adjCellCount)
-		{
-			agentMap[row][col].wumpProb = 1;
-			wumpusConfirmed(row, col);
-			return;
-		}
-
-		agentMap[row][col].wumpProb = ((double) stenchCount) / adjCellCount;
-	}
-
-/*
-********************Pit Detection Logic********************
-*/
-
-	private void pitConfirmed(int row, int col)
-	{
-		if(agentMap[row][col].pitProb != 1)
-			return;
-
-		int adjRow;
-		int adjCol;
-		for(int n = 0; n < 4; ++n)
-		{
-			adjRow = row + oscillationFunction(n);
-			adjCol = col + oscillationFunction(n + 1);
-			
-			if(adjRow < 0 || adjRow >= mapRows || adjCol < 0 || adjCol >= mapCols)
-				continue;
-			
-			agentMap[adjRow][adjCol].breeze = true;
-		}
-		
-		int adjRow1;
-		int adjCol1;
-		for(int m = 0; m < 4; ++m)
-		{
-			adjRow1 = row + oscillationFunction(m);
-			adjCol1 = col + oscillationFunction(m + 1);
-			
-			if(adjRow1 < 0 || adjRow1 >= mapRows || adjCol1 < 0 || adjCol1 >= mapCols)
-				continue;
-
-			int adjRow2;
-			int adjCol2;
-			for(int l = 0; l < 2; ++l)
-			{
-				adjRow2 = adjRow1 + oscillationFunction(m + l);
-				adjCol2 = adjCol1 + oscillationFunction(m + l + 1);
-		
-				if(adjRow2 < 0 || adjRow2 >= mapRows || adjCol2 < 0 || adjCol2 >= mapCols)
-					continue;
-			
-				updatePitProbability(adjRow2, adjCol2);
-			}
-		}
-	}
-
-	private void noPitConfirmed(int row, int col)
-	//Should only call on cells that have been confirmed as non-pits.
-	//This function performs some logical consistency checks on this world.
-	{
-//		System.out.printf("Cell: row %d; col %d\n", row, col);		
-		if(agentMap[row][col].pitProb != 0)
-			return;
-
-		int adjRow;
-		int adjCol;
-		for (int n = 0; n < 4; ++n)
-		{
-			adjRow = row + oscillationFunction(n);
-			adjCol = col + oscillationFunction(n + 1);
-			if(adjRow < 0 || adjRow >= mapRows || adjCol < 0 || adjCol >= mapCols)
-				continue;
-			
-//			System.out.printf("First Cells: row %d; col %d\n", adjRow1, adjCol1);
-
-			checkBreezeConsistency(adjRow, adjCol);
-		}
-	}
-
-	private void checkBreezeConsistency(int row, int col)
-	{
-		int potentialPitRow = -1;
-		int potentialPitCol = -1;
-		int potentialPitCount = 0;
-		
-		if(!agentMap[row][col].breeze)
-			return;
-				
-		int adjRow;
-		int adjCol;
-		for (int n = 0; n < 4; ++n)
-		{
-			adjRow = row + oscillationFunction(n);
-			adjCol = col + oscillationFunction(n + 1);
-
-			if(adjRow < 0 || adjRow >= mapRows || adjCol < 0 || adjCol >= mapCols)
-				continue;
-
-			if(agentMap[adjRow][adjCol].pitProb != 0)
-			{
-				potentialPitRow = adjRow;
-				potentialPitCol = adjCol;
-				++potentialPitCount;
-			}
-
-		}
-		if(potentialPitCount == 1)
-		{
-			agentMap[potentialPitRow][potentialPitCol].pitProb = 1;
-			pitConfirmed(potentialPitRow, potentialPitCol);		
-		}
-	}
-
-	private void updatePitProbability(int row, int col)
-	{
-		int adjCellCount = 0;
-		int breezeCount = 0;
-
-		if(agentMap[row][col].pitProb == 0 || agentMap[row][col].pitProb == 1)
-			return;
-
-		int adjRow;
-		int adjCol;
-		for (int n = 0; n < 4; ++n)
-		{
-			adjRow = row + oscillationFunction(n);
-			adjCol = col + oscillationFunction(n + 1);
-
-			if(adjRow < 0 || adjRow >= mapRows || adjCol < 0 || adjCol >= mapCols)
-				continue;
-
-			++adjCellCount;			
-			if(agentMap[adjRow][adjCol].breeze)
-				++breezeCount;
-
-			if(agentMap[adjRow][adjCol].explored && !agentMap[adjRow][adjCol].breeze)
-			{
-				agentMap[row][col].pitProb = 0;
-				noPitConfirmed(row, col);
-				return;
-			}
-//			System.out.printf("Cell: %d, %d\n", adjRow, adjCol);
-		}
-
-		if(breezeCount == adjCellCount)
-		{
-			agentMap[row][col].pitProb = 1;
-			pitConfirmed(row, col);
-			return;
-		}
-
-		agentMap[row][col].pitProb = ((double) breezeCount) / adjCellCount;
-	}
-
-	public void updateMap
-	(
-		boolean stench,
-		boolean breeze,
-		boolean bump
-	)
-	{
-		agentMap[curRow][curCol].pitProb = 0;
-		agentMap[curRow][curCol].wumpProb = 0;
-		agentMap[curRow][curCol].stench = stench;
-		agentMap[curRow][curCol].breeze = breeze;
-
-		// bump tells us that we hit a wall, record this number
-		if(bump)
-		{
-			if(curDir == 0)
-			{
-				mapCols = curCol + 1;
-				for(int row = 0; row < mapRows; ++row)
-				{
-					checkBreezeConsistency(row, curCol);
-					if(agentMap[row][curCol].pitProb > 0 && agentMap[row][curCol].pitProb < 1)
-						updatePitProbability(row, curCol);
-
-					if(!wumpusFound)
-					{
-						checkStenchConsistency(row, curCol);
-						if(agentMap[row][curCol].wumpProb > 0 && agentMap[row][curCol].wumpProb < 1)
-							updateWumpusProbability(row, curCol);
-					}
-				}
-			}
-			else if(curDir == 3)
-			{
-				mapRows = curRow + 1;
-				for(int col = 0; col < mapCols; ++col)
-				{
-					checkBreezeConsistency(curRow, col);
-					if(agentMap[curRow][col].pitProb > 0 && agentMap[curRow][col].pitProb < 1)
-						updatePitProbability(curRow, col);
-
-					if(!wumpusFound)
-					{
-						checkStenchConsistency(curRow, col);
-						if(agentMap[curRow][col].wumpProb > 0 && agentMap[curRow][col].wumpProb < 1)
-							updateWumpusProbability(curRow, col);
-					}
-				}
-			}
-			return;
-		}
-
-		// If previously explored
-	
-		if(breeze)
-		{
-
-			if(!agentMap[curRow][curCol].explored)
-			{
-				int adjRow;
-				int adjCol;
-				for (int n = 0; n < 4; ++n)
-				{
-					adjRow = curRow + oscillationFunction(n);
-					adjCol = curCol + oscillationFunction(n + 1);
-
-					if(adjRow < 0 || adjRow >= mapRows || adjCol < 0 || adjCol >= mapCols)
-						continue;
-
-					updatePitProbability(adjRow, adjCol);
-				}
-			}
-			checkBreezeConsistency(curRow, curCol);
-		}
-		else
-		{
-			if(!agentMap[curRow][curCol].explored)
-			{
-				int adjRow;
-				int adjCol;
-				for (int n = 0; n < 4; ++n)
-				{
-					adjRow = curRow + oscillationFunction(n);
-					adjCol = curCol + oscillationFunction(n + 1);
-
-					if(adjRow < 0 || adjRow >= mapRows || adjCol < 0 || adjCol >= mapCols)
-						continue;
-
-					agentMap[adjRow][adjCol].pitProb = 0;
-				}
-				for (int m = 0; m < 4; ++m)
-				{
-					adjRow = curRow + oscillationFunction(m);
-					adjCol = curCol + oscillationFunction(m + 1);
-
-					if(adjRow < 0 || adjRow >= mapRows || adjCol < 0 || adjCol >= mapCols)
-						continue;
-
-					noPitConfirmed(adjRow, adjCol);
-				}
-			}
-		}
-		if(!wumpusFound)
-		{
-			if(stench)
-			{
-				if(!agentMap[curRow][curCol].explored)
-				{
-					int adjRow;
-					int adjCol;
-					for (int n = 0; n < 4; ++n)
-					{
-						adjRow = curRow + oscillationFunction(n);
-						adjCol = curCol + oscillationFunction(n + 1);
-
-						if(adjRow < 0 || adjRow >= mapRows || adjCol < 0 || adjCol >= mapCols)
-							continue;
-
-						updateWumpusProbability(adjRow, adjCol);
-					}
-				}
-				checkStenchConsistency(curRow, curCol);
-			}
-		
-			else
-			{
-				if(!agentMap[curRow][curCol].explored)
-				{
-					int adjRow;
-					int adjCol;
-					for (int n = 0; n < 4; ++n)
-					{
-						adjRow = curRow + oscillationFunction(n);
-						adjCol = curCol + oscillationFunction(n + 1);
-
-						if(adjRow < 0 || adjRow >= mapRows || adjCol < 0 || adjCol >= mapCols)
-							continue;
-
-						agentMap[adjRow][adjCol].wumpProb = 0;
-					}
-					for (int m = 0; m < 4; ++m)
-					{
-						adjRow = curRow + oscillationFunction(m);
-						adjCol = curCol + oscillationFunction(m + 1);
-
-						if(adjRow < 0 || adjRow >= mapRows || adjCol < 0 || adjCol >= mapCols)
-							continue;
-
-						noWumpusConfirmed(adjRow, adjCol);
-					}
-				}
-			}
-		}
-
-		agentMap[curRow][curCol].explored = true;
-		noPitConfirmed(curRow, curCol);
-		noWumpusConfirmed(curRow, curCol);
-	}
 
 	//End Replace MA 10/30
+	private State forward(State curState)
+	{
+		if(curState.row >= mapRows || curState.col >= mapCols)
+			return new State();
+		
+		switch(curState.dir)
+		{
+			case 0:
+				return new State(curState.row, curState.col + 1, curState.dir);
+			case 1:
+				return new State(curState.row - 1, curState.col, curState.dir);
+			case 2:
+				return new State(curState.row, curState.col - 1, curState.dir);
+			case 3:
+				return new State(curState.row + 1, curState.col, curState.dir);
+			default:
+				return new State();
+		}
+		
+	}
+	private State turnLeft(State curState)
+	{
+		return new State(curState.row, curState.col, (curState.dir + 1) % 4);
+	}
+	private State turnRight(State curState)
+	{
+		return new State(curState.row, curState.col, (curState.dir - 1) % 4);
+	}
+
+	private Stack<State> recursiveSearch(State curState, int depth)
+	{
+		Stack<State> tempStack;
+		System.out.println(curState.toString());
+		if(goalCells.contains(new SimpleCell(curState.row, curState.col)))
+		{
+			System.out.println("I choose: ");
+			System.out.println(curState.toString());
+			tempStack = new Stack<State>();
+			tempStack.add(curState);
+			return tempStack;
+		}	
+
+		if(	depth == 0 || 
+			curState.row < 0 || 
+			curState.col < 0 || 
+			agentMap[curState.row][curState.col].pitProb != 0|| 
+			agentMap[curState.row][curState.col].wumpProb != 0)
+			return new Stack<State>();
+
+		tempStack = recursiveSearch(forward(curState), depth - 1);
+		if(!tempStack.empty())
+		{
+			tempStack.add(curState);
+			return tempStack;
+		}
+		tempStack = recursiveSearch(turnRight(curState), depth - 1);
+		if(!tempStack.empty())
+		{
+			tempStack.add(curState);
+			return tempStack;
+		}
+		tempStack = recursiveSearch(turnLeft(curState), depth - 1);
+		if(!tempStack.empty())
+		{
+			tempStack.add(curState);
+			return tempStack;
+		}
+		
+		return new Stack<State>();
+	}
+
+	private Stack<State> iterDeep(int row, int col, int dir)
+	{
+		Stack<State> tempStack = new Stack<State>();
+		for(int depth = 0; depth < 10; ++depth)
+		{
+			System.out.println("Died yet?");
+			tempStack = recursiveSearch(new State(row, col, dir), depth);
+			if(!tempStack.empty())
+				return tempStack;
+		}
+		return new Stack<State>();
+	}
+/*
+	private boolean goToGoalCell(boolean exit){
+		if(exit)
+		{
+			goalCells.clear();
+			goalCells.add(new SimpleCell(0, 0));
+		}
+
+		commandQueue.clear();
+		
+		commandQueue.addAll(iterDeep(curRow, curCol, curDir));
+
+		if(exit)
+		{
+			commandQueue.add(3);
+		}
+	}
+*/
 	public void printAgentMap()
 	{
-		for(int row = mapRows-1; row >=0; --row)
+		for(int row = mapRows - 1; row >=0; --row)
 		{
 			for(int col = 0; col < mapCols; ++col)
 			{
-				System.out.printf("%7.2f,%-5.2f", agentMap[row][col].getPitProb(),agentMap[row][col].getWumpProb());
+				System.out.printf("%7.2f%5.2f", agentMap[row][col].pitProb, agentMap[row][col].wumpProb);
 			}
 			System.out.println();
 		}
 	}
-	
+
+	public void printSUC()
+	{
+		for(SimpleCell cell : goalCells)
+		{
+			System.out.println(cell.toString());
+		}
+	}
+
 	
 	
 	private Stack<Action> goUp(){
